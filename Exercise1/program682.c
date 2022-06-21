d9a0f5537727e4a8e35da22f30aa56dcfc140221
Çağla Nur Yuva <87543468+CaglaNurYuva@users.noreply.github.com>
2022-06-21 19:24:42 +0300

    Add files via upload

--- Exercise1/program682.c
+++ Exercise1/program682.c
@@ -1,90 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int longestPalindrom(char *str);
+void printing(char *str, int low, int high);
 
-/* Correct solution 1 */
 int main() {
-    char username[25];
-    char resultant[25] = "";
-    int i,j,flag=0, count=0, index=0;
+    char *string = (char*)calloc(30,sizeof(char));
+  
+    printf("Enter string: ");
+    scanf("%s",string);
 
-    printf("Enter username: ");
-    scanf("%s",username);
+    printf("\nLongest palindrom's length = %d\n",longestPalindrom(string));
+    free(string);
+ 
+    return 0;
+}
 
-    for(i=0; username[i] != '\0'; i++){
+int longestPalindrom(char *str) {
+    int length = strlen(str);
+    int maxLength = 1, start=0, low, high, i, sub_palindrom_length;
 
-        j=0;
-        flag=1;
-        while(resultant[j] != '\0'){
-            if(username[i] == resultant[j]) {
-                flag=0;
-            }
-            j++;
+    if(length < 2) return length;
+    for(i=0; i<length; i++){
+        low = i-1;
+        high = i+1;
+
+        while(high<length && str[high] == str[i]) high++;
+        while(low>=0 && str[low] == str[i]) low--;
+        while(low >= 0 && high<length && str[low] == str[high]) {
+            low--;
+            high++;
         }
 
-        if(flag == 1) {
-            resultant[index] = username[i];
-            index++;
-            count++;
+        sub_palindrom_length = high-low-1; 
+        if(maxLength < sub_palindrom_length){
+            maxLength = sub_palindrom_length;
+            start = low+1;
         }
+
     }
 
-    if(count%2 == 1) printf("IGNORE HIM!");
-    else printf("CHAT WITH HER!");
-    return 0;
+    printf("Longest subpalindrome = ");
+    printing(str,start,start+maxLength-1);
+    return maxLength;
 
 }
 
 
-
-/* Correct solution 2 */
-int main() {
-    char array[55] = "sevenkplus";
-    int alphabets[55];
-    int i,j, nums_odd=0;
-
-    for(i=0; i<26; i++){
-        alphabets[i] = 0;
+void printing(char *str, int low, int high) {
+    int i;
+    for(i=low; i<=high; ++i){
+        printf("%c",str[i]);
     }
-
-    for(i=0; array[i] != '\0'; i++){
-        ++alphabets[array[i] - 'a'];
-        if(alphabets[array[i] - 'a'] == 1){
-           
-            nums_odd++;
-        }
-    }
-
-
-    if(nums_odd%2 == 1) printf("IGNORE HIM!");
-    else printf("CHAT WITH HER!");
-    return 0;
 }
 
 
 
-
-/* Wrong attempt */
-/*int main() {
-    char array[25] = "xiaodao";
-    int *help = (int*)calloc(25,sizeof(int));
-    int count=0, index=0, length=0;
-
-    while(array[index] != '\0'){
-        ++help[index];
-        index++;
-    }
-
-    while(count != index){
-        if(help[count] >= 2) {
-            printf("IGNORE HIM!");
-            return 0;
-        }
-        count++;
-
-    }
-    
-    printf("CHAT WITH HER!");
-    return 0;
-
-    
-}*/
